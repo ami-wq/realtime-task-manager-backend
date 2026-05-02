2d60c1dca59e60e15f8fe71bbd8d419ce4f0e317
Ami <marinaskv083@gmail.com>
2026-05-02 15:35:04 +0300

    feat: parse incoming WebSocket messages

--- src/websocket/wsServer.ts
+++ src/websocket/wsServer.ts
@@ -1,11 +1,34 @@
 import type http from "node:http";
-import { WebSocketServer } from "ws";
+import { type WebSocket, WebSocketServer } from "ws";
+
+import type { WSMessage } from "../types/ws";
 
 function createWebSocketServer(server: http.Server) {
   const wss = new WebSocketServer({ server });
 
-  wss.on("connection", () => {
+  wss.on("connection", (ws: WebSocket) => {
     console.log("WebSocket connection established");
+
+    ws.on("message", data => {
+      let parsed: WSMessage;
+
+      try {
+        parsed = JSON.parse(data.toString());
+      } catch (_err) {
+        console.error("[WS] Invalid JSON");
+
+        ws.send(
+          JSON.stringify({
+            type: "error",
+            payload: "Invalid JSON format",
+          }),
+        );
+
+        return;
+      }
+
+      console.log("[WS] Received:", parsed);
+    });
   });
 
   return wss;
