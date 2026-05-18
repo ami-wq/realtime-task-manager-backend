a6f83f9870f3ba8b87fdef3ba19ba13d4b6c6977
Ami <marinaskv083@gmail.com>
2026-05-18 16:00:11 +0300

    feat: implement inter-service event communication

--- services/task-service/src/routes/task.route.ts
+++ services/task-service/src/routes/task.route.ts
@@ -10,7 +10,7 @@ router.get("/tasks", (__, res) => {
   res.json(tasks);
 });
 
-router.post("/tasks", (req, res) => {
+router.post("/tasks", async (req, res) => {
   const { title } = req.body;
 
   const newTask: Task = {
@@ -20,6 +20,26 @@ router.post("/tasks", (req, res) => {
   };
 
   tasks.push(newTask);
+
+  try {
+    await fetch("http://localhost:5001/events", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "TASK_CREATED",
+        payload: {
+          taskId: newTask.id,
+          title: newTask.title,
+        },
+      }),
+    });
+
+    console.log("[EVENT SENT] TASK_CREATED");
+  } catch (error) {
+    console.error("[EVENT ERROR]", error);
+  }
 
   res.status(201).json(newTask);
 });
